e02f3b44cda368dbb9bb66fc83345046f7b19de9
Jesse Wolgamott <jesse@comalproductions.com>
2013-06-10 17:33:10 -0500

    Fix: dalli should have been in gemfile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @languages = Rails.cache.fetch("lang") do
+    @languages = Rails.cache.fetch("lang", expires_in: 5.minutes) do
       Language.all
     end
   end
